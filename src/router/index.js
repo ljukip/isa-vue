7c5d2abc416fb1a53705fff3e7e306db8048b79f
minales <inamales@gmail.com>
2021-02-02 19:23:42 +0100

    pharmacistProfile,pharmacistHome,dermatologistHome,dermatologistProfile front

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import UserProfile from "../views/UserProfile.vue";
+import PharmacistProfile from "../views/PharmacistProfile.vue";
+import DermatologistProfile from "../views/DermatologistProfile.vue";
 import UserHome from "../views/UserHome.vue";
+import PharmacistHome from "../views/PharmacistHome.vue";
+import DermatologistHome from "../views/DermatologistHome.vue";
 
 Vue.use(VueRouter);
 
@@ -21,6 +25,26 @@ const routes = [
     path: "/userHome",
     name: "UserHome",
     component: UserHome
+  },
+  {
+    path: "/pharmacistProfile",
+    name: "PharmacistProfile",
+    component: PharmacistProfile
+  },
+  {
+    path: "/dermatologistProfile",
+    name: "DermatologistProfile",
+    component: DermatologistProfile
+  },
+  {
+    path: "/dermatologistHome",
+    name: "DermatologistHome",
+    component: DermatologistHome
+  },
+  {
+    path: "/pharmacistHome",
+    name: "PharmacistHome",
+    component: PharmacistHome
   },
   {
     path: "/about",
---
+++ src/views/PharmacistProfile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <PharmacistProfileComponent />
+  </div>
+</template>
+
+<script>
+import PharmacistProfileComponent from "@/components/pharmacistProfile.vue";
+export default {
+  name: "PharmacistProfile",
+  components: { PharmacistProfileComponent },
+};
+</script>
+
+<style scoped>
+</style>
